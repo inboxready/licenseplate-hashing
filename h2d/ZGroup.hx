9669d1f3c298249bc4990169d9b37422a22f5b09
inboxready <112681018+inboxready@users.noreply.github.com>
2020-09-15 15:05:10 +0000

    buggy

--- h2d/ZGroup.hx
+++ h2d/ZGroup.hx
@@ -6,4 +6,20 @@ private class State {
 	public var depthTest   : h3d.mat.Data.Compare;
 	public var front2back  : Bool;
 	public var killAlpha   : Bool;
-	public var onBeginDraw : h2d.D
+	public var onBeginDraw : h2d.Drawable->Bool;
+
+	public function new() { }
+
+	public function loadFrom( ctx : RenderContext ) {
+		depthWrite  = ctx.pass.depthWrite;
+		depthTest   = ctx.pass.depthTest;
+		front2back  = ctx.front2back;
+		killAlpha   = ctx.killAlpha;
+		onBeginDraw = ctx.onBeginDraw;
+	}
+
+	public function applyTo( ctx : RenderContext ) {
+		ctx.pass.depth(depthWrite, depthTest);
+		ctx.front2back  = front2back;
+		ctx.killAlpha   = killAlpha;
+		c
