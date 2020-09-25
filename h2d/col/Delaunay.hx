666f77862b2871e76daf3aeb7afa71994af4bb0f
inboxready <112681018+inboxready@users.noreply.github.com>
2020-09-25 14:59:13 +0000

    zram: Fix sparse warnings

--- h2d/col/Delaunay.hx
+++ h2d/col/Delaunay.hx
@@ -10,4 +10,28 @@ class DelaunayTriangle  {
 	/** Second vertex of the triangle. **/
 	public var p2:Point;
 	/** Third vertex of the triangle. **/
-	public var p3:P
+	public var p3:Point;
+	/** Create a new Delaunay result triangle. **/
+	public function new(p1,p2,p3) {
+		this.p1 = p1;
+		this.p2 = p2;
+		this.p3 = p3;
+	}
+}
+
+private class DelaunayEdge  {
+	public var p1:Point;
+	public var p2:Point;
+	public function new(p1, p2) {
+		this.p1 = p1;
+		this.p2 = p2;
+	}
+	public inline function equals(e:DelaunayEdge) {
+		return (p1 == e.p1 && p2 == e.p2) || (p1 == e.p2 && p2 == e.p1);
+	}
+}
+
+/**
+	A Delaunay triangulation utility. See `Delaunay.triangulate`.
+**/
+class De
