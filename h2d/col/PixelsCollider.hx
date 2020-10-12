40e83401e61617037c15ed7341353b42ebf3e03b
inboxready <112681018+inboxready@users.noreply.github.com>
2020-10-12 16:13:34 +0000

    Release version 1.2.0

--- h2d/col/PixelsCollider.hx
+++ h2d/col/PixelsCollider.hx
@@ -1,4 +1,19 @@
 package h2d.col;
 
 /**
-	An `hxd.Pixels`-based collider. Checks for pixel 
+	An `hxd.Pixels`-based collider. Checks for pixel color value under point to be above the cutoff value.
+
+	Note that it checks as `channel > cutoff`, not `channel >= cutoff`, hence cutoff value of 255 would never pass the test.
+**/
+class PixelsCollider implements Collider {
+
+	/**
+		The source pixel data which is tested against.
+	**/
+	public var pixels : hxd.Pixels;
+
+	/**
+		The red channel cutoff value in range of -1...255
+
+		Set to 255 to always fail the test.
+		@d
