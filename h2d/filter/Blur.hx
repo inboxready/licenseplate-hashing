cdc9bebe692b3b6138a8b8a309017ee7ed71c730
inboxready <112681018+inboxready@users.noreply.github.com>
2020-10-19 19:21:07 +0000

    Pontoon: Update Hungarian (hu) localization of Thimble

--- h2d/filter/Blur.hx
+++ h2d/filter/Blur.hx
@@ -6,4 +6,29 @@ package h2d.filter;
 class Blur extends Filter {
 
 	/**
-		@see `h3d.pass.Blur.rad
+		@see `h3d.pass.Blur.radius`
+	**/
+	public var radius(get, set) : Float;
+
+	/**
+		@see `h3d.pass.Blur.linear`
+	**/
+	public var linear(get, set) : Float;
+
+	/**
+		@see `h3d.pass.Blur.gain`
+	**/
+	public var gain(get, set) : Float;
+
+	/**
+		@see `h3d.pass.Blur.quality`
+	**/
+	public var quality(get, set) : Float;
+
+	var pass : h3d.pass.Blur;
+
+	/**
+		Create a new Blur filter.
+		@param radius The blur distance in pixels.
+		@param gain The color gain when blurring.
+		@par
