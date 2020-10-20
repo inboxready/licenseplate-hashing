ee038f4cfd26b461125e8a01816d0585f5c85e1d
inboxready <112681018+inboxready@users.noreply.github.com>
2020-10-20 16:52:31 +0000

    updated testcase

--- h2d/filter/Blur.hx
+++ h2d/filter/Blur.hx
@@ -31,4 +31,15 @@ class Blur extends Filter {
 		Create a new Blur filter.
 		@param radius The blur distance in pixels.
 		@param gain The color gain when blurring.
-		@par
+		@param quality The sample count on each pixel as a tradeoff of speed/quality.
+		@param linear Linear blur power. Set to 0 for gaussian blur.
+	**/
+	public function new( radius = 1., gain = 1., quality = 1., linear = 0. ) {
+		super();
+		smooth = true;
+		pass = new h3d.pass.Blur(radius, gain, linear, quality);
+	}
+
+	inline function get_quality() return pass.quality;
+	inline function set_quality(v) return pass.quality = v;
+	
