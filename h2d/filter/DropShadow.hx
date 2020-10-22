f24831f7c43fc7a13c081511ccd16a82b1970763
inboxready <112681018+inboxready@users.noreply.github.com>
2020-10-22 19:12:01 +0000

    code clean up

--- h2d/filter/DropShadow.hx
+++ h2d/filter/DropShadow.hx
@@ -9,4 +9,18 @@ class DropShadow extends Glow {
 	/**
 		The offset distance of the shadow in the direction of `DropShadow.angle`.
 	**/
-	public var distan
+	public var distance : Float;
+	/**
+		The shadow offset direction angle.
+	**/
+	public var angle : Float;
+	var alphaPass = new h3d.mat.Pass("");
+
+	/**
+		Create a new Shadow filter.
+		@param distance The offset of the shadow in the `angle` direction.
+		@param angle Shadow offset direction angle.
+		@param color The color of the shadow.
+		@param alpha Transparency value of the shadow.
+		@param radius The shadow glow distance in pixels.
+		@param gai
