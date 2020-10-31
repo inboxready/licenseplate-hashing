2dec3b91536121c960dab70078683ffd828d67d7
inboxready <112681018+inboxready@users.noreply.github.com>
2020-10-31 13:40:30 +0000

    Adapt ChangeLog

--- h2d/filter/Shader.hx
+++ h2d/filter/Shader.hx
@@ -5,4 +5,15 @@ package h2d.filter;
 
 	Provides an easy interface to implement custom filters without going too deep into filter rendering process with render passes.
 
-	Compatible shaders shou
+	Compatible shaders should extend from `h3d.shader.ScreenShader` and contain an input texture uniform, as well as assign `pixelColor` in fragment shader.
+
+	Sample of a simple custom filter:
+	```haxe
+	class InvertColorShader extends h3d.shader.ScreenShader {
+		static var SRC = {
+			@param var texture : Sampler2D;
+
+			function fragment() {
+				var pixel : Vec4 = texture.get(calculatedUV);
+				// Premultiply alpha to ensure correct transparency.
+				pixelColor = vec4(
