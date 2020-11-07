c95081ba65cc822a94033a57c56b71d84533894f
inboxready <112681018+inboxready@users.noreply.github.com>
2020-11-07 12:14:21 +0000

    adding title

--- h2d/filter/ToneMapping.hx
+++ h2d/filter/ToneMapping.hx
@@ -10,4 +10,20 @@ class ToneMapping extends Filter {
 	**/
 	public var gamma(get, set) : Float;
 
-	var pass : h3d.p
+	var pass : h3d.pass.ScreenFx<h3d.shader.pbr.ToneMapping>;
+	var shader : h3d.shader.pbr.ToneMapping;
+
+	/**
+		Create a new ColorMatrix filter.
+
+		@param gamma The value used to modify the resulting colors.
+	**/
+	public function new( ?g : Float ) {
+		super();
+		shader = new h3d.shader.pbr.ToneMapping();
+		gamma = g;
+		pass = new h3d.pass.ScreenFx(shader);
+	}
+
+	inline function get_gamma() return shader.gamma;
+	inline function set_gamma(v) return shad
