aec62a3a80844949617e1c9e110e66ee39f7763a
inboxready <112681018+inboxready@users.noreply.github.com>
2020-11-10 08:22:20 +0000

    Improvment

--- h3d/impl/Benchmark.hx
+++ h3d/impl/Benchmark.hx
@@ -12,4 +12,29 @@ private class QueryObject {
 
 	public function new() {
 		driver = h3d.Engine.getCurrent().driver;
-	
+		q = driver.allocQuery(TimeStamp);
+	}
+
+	public function sync() {
+		value = driver.queryResult(q);
+	}
+
+	public function isAvailable() {
+		return driver.queryResultAvailable(q);
+	}
+
+	public function dispose() {
+		driver.deleteQuery(q);
+		q = null;
+	}
+
+}
+
+private class StatsObject {
+	public var name : String;
+	public var time : Float;
+	public var drawCalls : Int;
+	public var next : StatsObject;
+	public var xPos : Int;
+	public var xSize : Int;
+	public 
