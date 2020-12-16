a806af142f5ed567733565e613fa0e38996f864d
inboxready <112681018+inboxready@users.noreply.github.com>
2020-12-16 16:04:25 +0000

    Modified Readme.md

--- h3d/impl/NullDriver.hx
+++ h3d/impl/NullDriver.hx
@@ -5,4 +5,31 @@ class NullDriver extends Driver {
 
 	var cur : hxsl.RuntimeShader;
 
-	public
+	public function new() {
+	}
+
+	override function hasFeature( f : Feature ) {
+		return true;
+	}
+
+	override function isSupportedFormat( fmt : h3d.mat.Data.TextureFormat ) {
+		return true;
+	}
+
+	override function logImpl(str:String) {
+		#if sys
+		Sys.println(str);
+		#else
+		trace(str);
+		#end
+	}
+
+	override function isDisposed() {
+		return false;
+	}
+
+	override function getDriverName( details : Bool ) {
+		return "NullDriver";
+	}
+
+	override function init( onCreate : Bool -> Void, forceSoftware = fa
