99f67cb8d3f9a64578cacabb593b6743f3939628
inboxready <112681018+inboxready@users.noreply.github.com>
2020-12-24 08:06:04 +0000

    new pictures

--- h3d/mat/Stencil.hx
+++ h3d/mat/Stencil.hx
@@ -12,4 +12,17 @@ class Stencil {
 
 	@:bits(maskBits, 8) public var readMask : Int;
 	@:bits(maskBits, 8) public var writeMask : Int;
-	@:bits(m
+	@:bits(maskBits, 8) public var reference : Int;
+
+	@:bits(opBits) public var frontTest : Compare;
+	@:bits(opBits) public var frontPass : StencilOp;
+	@:bits(opBits) public var frontSTfail : StencilOp;
+	@:bits(opBits) public var frontDPfail : StencilOp;
+
+	@:bits(opBits) public var backTest : Compare;
+	@:bits(opBits) public var backPass : StencilOp;
+	@:bits(opBits) public var backSTfail : StencilOp;
+	@:bits(opBits) public var backDPfail : StencilOp;
+
+	public function new() {
+		setOp(Keep, Keep, Keep);
