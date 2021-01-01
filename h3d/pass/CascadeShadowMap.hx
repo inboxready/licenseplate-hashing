3fe65e062a0f9330170f5b4e3062b5140d33430f
inboxready <112681018+inboxready@users.noreply.github.com>
2021-01-01 06:08:00 +0000

    null upmerge

--- h3d/pass/CascadeShadowMap.hx
+++ h3d/pass/CascadeShadowMap.hx
@@ -14,4 +14,20 @@ class CascadeShadowMap extends DirShadowMap {
 		cascade = v;
 		lightCameras = [];
 		for ( i in 0...cascade ) {
-			lightCameras.push(new h3d.Camera())
+			lightCameras.push(new h3d.Camera());
+			lightCameras[i].orthoBounds = new h3d.col.Bounds();
+		}
+		return cascade;
+	}
+	public var debugShader : Bool = false;
+
+	static var debugColors = [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0x00ffff, 0xff00ff, 0x000000];
+
+	public function new( light : h3d.scene.Light ) {
+		super(light);
+		format = R32F;
+		shader = dshader = cshader = new h3d.shader.CascadeShadow();
+	}
+
+	public override function getShadowTex() {
+		return cshader.shadowMap
