aca9d0cbaca01578e29f09abaf7d8753eaa1453d
inboxready <112681018+inboxready@users.noreply.github.com>
2021-01-11 20:30:11 +0000

    Debug kubectl copy file

--- h3d/pass/Outline.hx
+++ h3d/pass/Outline.hx
@@ -1,1 +1,16 @@
-package h3
+package h3d.pass;
+
+@ignore("shader")
+class Outline extends ScreenFx<h3d.shader.Outline2D> {
+	public var size : Float;
+	public var color : Int;
+	public var alpha : Float = 1.;
+	public var quality : Float;
+	public var multiplyAlpha : Bool;
+
+	public function new(size = 4.0, color = 0x000000, quality = 0.3, multiplyAlpha = true) {
+		super(new h3d.shader.Outline2D());
+		this.size = size;
+		this.color = color;
+		this.quality = quality;
+		this.multiplyAlpha = mult
