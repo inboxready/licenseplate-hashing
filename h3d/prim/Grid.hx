2e094e01dd4e69f3e66dc97b27802f905f507c2c
inboxready <112681018+inboxready@users.noreply.github.com>
2021-01-15 16:42:19 +0000

    Setting version to 0.3.1

--- h3d/prim/Grid.hx
+++ h3d/prim/Grid.hx
@@ -1,3 +1,17 @@
 package h3d.prim;
 
-class Grid extends Polygon
+class Grid extends Polygon {
+
+	public var width (default, null) : Int;
+	public var height (default, null)  : Int;
+	public var cellWidth (default, null) : Float;
+	public var cellHeight (default, null)  : Float;
+
+	public function new( width : Int, height : Int, cellWidth = 1., cellHeight = 1. ) {
+		this.width = width;
+		this.height = height;
+		this.cellWidth = cellWidth;
+		this.cellHeight = cellHeight;
+
+		var idx = new hxd.IndexBuffer();
+		for( y in 0
