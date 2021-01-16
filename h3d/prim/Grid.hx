8b7e8f87c560d22edd1c9f60ce73cd905a8bf13f
inboxready <112681018+inboxready@users.noreply.github.com>
2021-01-16 17:11:06 +0000

    golint

--- h3d/prim/Grid.hx
+++ h3d/prim/Grid.hx
@@ -14,4 +14,23 @@ class Grid extends Polygon {
 		this.cellHeight = cellHeight;
 
 		var idx = new hxd.IndexBuffer();
-		for( y in 0
+		for( y in 0...height )
+			for( x in 0...width ) {
+				var s = x + y * (width + 1);
+				idx.push(s);
+				idx.push(s + 1);
+				idx.push(s + width + 1);
+				idx.push(s + 1);
+				idx.push(s + width + 2);
+				idx.push(s + width + 1);
+			}
+		super(
+			[for( y in 0...height + 1 ) for( x in 0...width + 1 ) new h3d.col.Point(x * cellWidth, y * cellHeight, 0)],
+			idx
+		);
+	}
+
+	override function addUVs() {
+		uvs = [];
+		for(i in 0 ... points.length){
+			var y = hxd.Mat
