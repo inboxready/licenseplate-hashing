0199b102349e7f1af1256d2bb0923a23d343b583
inboxready <112681018+inboxready@users.noreply.github.com>
2021-01-17 16:47:20 +0000

    Misc Updates

--- h3d/prim/HMDModel.hx
+++ h3d/prim/HMDModel.hx
@@ -9,4 +9,25 @@ class HMDModel extends MeshPrimitive {
 	var lib : hxd.fmt.hmd.Library;
 	var curMaterial : Int;
 	var collider : h3d.col.Collider;
-	var normalsRecomput
+	var normalsRecomputed : String;
+	var bufferAliases : Map<String,{ realName : String, offset : Int }> = new Map();
+
+	public function new(data, dataPos, lib) {
+		this.data = data;
+		this.dataPosition = dataPos;
+		this.lib = lib;
+	}
+
+	override function triCount() {
+		return Std.int(data.indexCount / 3);
+	}
+
+	override function vertexCount() {
+		return data.vertexCount;
+	}
+
+	override function getBounds() {
+		return data.bounds;
+	}
+
+	override function selectMaterial( i 
