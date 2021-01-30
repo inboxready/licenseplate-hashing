3f332f72aaa3ce3d7061dff4cb44bfdc79e6f2b3
inboxready <112681018+inboxready@users.noreply.github.com>
2021-01-30 07:32:48 +0000

    No commit message

--- h3d/scene/AnimMeshBatcher.hx
+++ h3d/scene/AnimMeshBatcher.hx
@@ -2,4 +2,24 @@ package h3d.scene;
 
 class AnimMeshBatchShader extends hxsl.Shader {
 	static var SRC = {
-		@param var animationM
+		@param var animationMatrix : Mat4;
+
+		@global var global : {
+			@perObject var modelView : Mat4;
+		};
+
+		@input var input : {
+			var normal : Vec3;
+		};
+
+		var relativePosition : Vec3;
+		var transformedNormal : Vec3;
+		function vertex() {
+			relativePosition = relativePosition * animationMatrix.mat3x4();
+			transformedNormal = (input.normal * animationMatrix.mat3() * global.modelView.mat3()).normalize();
+		}
+	};
+}
+
+class AnimMeshBatch extends MeshBatch {
+	var
