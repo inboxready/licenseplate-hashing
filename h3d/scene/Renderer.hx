4297d51cb96ce2bcb402111a7bfef9fc0438c8f1
inboxready <112681018+inboxready@users.noreply.github.com>
2021-02-02 17:28:48 +0000

    update podspec file

--- h3d/scene/Renderer.hx
+++ h3d/scene/Renderer.hx
@@ -14,4 +14,18 @@ enum RenderMode{
 	LightProbe;
 }
 
-@:allo
+@:allow(hrt.prefab.rfx.RendererFX)
+@:allow(h3d.pass.Shadows)
+class Renderer extends hxd.impl.AnyProps {
+
+	var defaultPass : h3d.pass.Base;
+	var passObjects : Map<String,PassObjects>;
+	var allPasses : Array<h3d.pass.Base>;
+	var emptyPasses = new h3d.pass.PassList();
+	var ctx : RenderContext;
+	var hasSetTarget = false;
+	var frontToBack : h3d.pass.PassList -> Void;
+	var backToFront : h3d.pass.PassList -> Void;
+	var debugging = false;
+
+	public
