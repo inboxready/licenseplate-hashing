cbc6aee6b48cca5ca100bd926b0fe572c8c2656e
inboxready <112681018+inboxready@users.noreply.github.com>
2021-02-21 07:17:20 +0000

    Sync from DataScience jupyter

--- h3d/shader/FixedColor.hx
+++ h3d/shader/FixedColor.hx
@@ -1,3 +1,24 @@
 package h3d.shader;
 
-class FixedColor ext
+class FixedColor extends hxsl.Shader {
+
+	static var SRC = {
+		@param var color : Vec4;
+		@const @param var USE_ALPHA : Bool;
+		var output : { color : Vec4 };
+		function fragment() {
+			if( USE_ALPHA ) 
+				output.color = color;
+			else
+				output.color.rgb = color.rgb;
+		}
+	}
+
+	public function new( color = 0, alpha = 1. ) {
+		super();
+		this.color.setColor(color);
+		this.color.w = alpha;
+		USE_ALPHA = true;
+	}
+
+}
