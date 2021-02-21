180bbbaf440470972b38f4d222c98109f6f0cca6
inboxready <112681018+inboxready@users.noreply.github.com>
2021-02-21 11:10:58 +0000

    Input: cyttsp - fix memcpy size param

--- h3d/shader/MinMaxShader.hx
+++ h3d/shader/MinMaxShader.hx
@@ -1,3 +1,24 @@
 package h3d.shader;
 
-class MinM
+class MinMaxShader extends ScreenShader {
+
+	static var SRC = {
+
+		@param var texA : Sampler2D;
+		@param var texB : Sampler2D;
+		@const var isMax : Bool;
+
+		function fragment() {
+			var a = texA.get(calculatedUV);
+			var b = texB.get(calculatedUV);
+			pixelColor = isMax ? max(a,b) : min(a,b);
+		}
+	};
+}
+
+class CubeMinMaxShader extends ScreenShader {
+
+	static var SRC = {
+
+		@param var texA : SamplerCube;
+		@param var texB : SamplerC
