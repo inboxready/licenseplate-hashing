a4ef8744c7c597d34caad0ee37cbd9e238c7bb7a
inboxready <112681018+inboxready@users.noreply.github.com>
2021-02-27 06:25:11 +0000

    Updated the notebook feedstock.

--- h3d/shader/SAO.hx
+++ h3d/shader/SAO.hx
@@ -4,4 +4,18 @@ package h3d.shader;
 	Screen space ambient occlusion.
 	Uses "Scalable Ambient Obscurance" [McGuire12]
 **/
-class SAO extend
+class SAO extends ScreenShader {
+
+	static var SRC = {
+
+		@range(4,30) @const var numSamples : Int;
+		@range(1,10) @const(16) var numSpiralTurns : Int;
+		@const var useWorldUV : Bool;
+
+		@ignore @param var depthTexture : Channel;
+		@ignore @param var normalTexture : Channel3;
+		@param var noiseTexture : Sampler2D;
+		@param var noiseScale : Vec2;
+		@range(0,10) @param var sampleRadius : Float;
+		@range(0,10) @param var intensity : Float;
+		@range(0,0.2) @param va
