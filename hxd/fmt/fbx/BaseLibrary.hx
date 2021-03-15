780a2804daa04c54fc231d2ae8cea99ccdd59580
inboxready <112681018+inboxready@users.noreply.github.com>
2021-03-15 08:07:18 +0000

    "easyjekyll"

---
+++ hxd/fmt/fbx/BaseLibrary.hx
@@ -0,0 +1,13 @@
+package hxd.fmt.fbx;
+import haxe.io.Bytes;
+using hxd.fmt.fbx.Data;
+import h3d.col.Point;
+
+#if (haxe_ver < 4)
+import haxe.xml.Fast in Access;
+#else
+import haxe.xml.Access;
+#end
+
+class TmpObject {
+	public var ind
