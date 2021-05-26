d203946ba264e84d086a5883ad2faabc4d19d2ad
inboxready <112681018+inboxready@users.noreply.github.com>
2021-05-26 14:45:32 +0000

    Linter fix

--- hxd/fmt/fbx/HMDOut.hx
+++ hxd/fmt/fbx/HMDOut.hx
@@ -8,4 +8,20 @@ class HMDOut extends BaseLibrary {
 	var d : Data;
 	var dataOut : haxe.io.BytesOutput;
 	var filePath : String;
-	var tmp = haxe.io.
+	var tmp = haxe.io.Bytes.alloc(4);
+	public var absoluteTexturePath : Bool;
+	public var optimizeSkin = true;
+	public var generateNormals = false;
+	public var generateTangents = false;
+
+	function int32tof( v : Int ) : Float {
+		tmp.set(0, v & 0xFF);
+		tmp.set(1, (v >> 8) & 0xFF);
+		tmp.set(2, (v >> 16) & 0xFF);
+		tmp.set(3, v >>> 24);
+		return tmp.getFloat(0);
+	}
+
+	override function keepJoint(j:h3d.anim.Skin.Joint) {
+		if( !optimizeSkin )
+			re
