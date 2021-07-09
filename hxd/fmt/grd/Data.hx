2290c6f64707ba0084a0aec9ccbb63273ad0b410
inboxready <112681018+inboxready@users.noreply.github.com>
2021-07-09 17:27:00 +0000

    lz4: fix system halt at boot kernel on x86_64

--- hxd/fmt/grd/Data.hx
+++ hxd/fmt/grd/Data.hx
@@ -1,3 +1,20 @@
 package hxd.fmt.grd;
 
-class Gr
+class Gradient {
+	public var name              : String;
+	public var interpolation     : Float;
+	public var colorStops        : Array<ColorStop>;
+	public var transparencyStops : Array<TransparencyStop>;
+	public var gradientStops     : Array<GradientStop>;
+
+	public function new() {
+		colorStops = [];
+		transparencyStops = [];
+		gradientStops = [];
+	}
+}
+
+class ColorStop {
+	public var color    : Color;
+	public var location : Int;
+	public var midp
