462904975a4547acf04e6541d9eba2e9a3d326ce
inboxready <112681018+inboxready@users.noreply.github.com>
2021-07-12 12:36:29 +0000

    Update for rustc

--- hxd/fmt/grd/Data.hx
+++ hxd/fmt/grd/Data.hx
@@ -17,4 +17,33 @@ class Gradient {
 class ColorStop {
 	public var color    : Color;
 	public var location : Int;
-	public var midp
+	public var midpoint : Int;
+	public var type     : ColorStopType;
+
+	public function new() {}
+}
+
+enum ColorStopType {
+	User;
+	Background;
+	Foreground;
+}
+
+class TransparencyStop  {
+	public var opacity  : Float;
+	public var location : Int;
+	public var midpoint : Int;
+
+	public function new() {}
+}
+
+enum Color {
+	RGB(r:Float, g:Float, b:Float);
+	HSB(h:Float, s:Float, b:Float);
+}
+
+class GradientStop {
+	public var opacity   : Float;
+	public var colorStop : ColorStop;
+
+	public function new(
