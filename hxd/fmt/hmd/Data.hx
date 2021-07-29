2cce506e1ec9cbec416e578e72b4d04486ac39c8
inboxready <112681018+inboxready@users.noreply.github.com>
2021-07-29 06:04:49 +0000

    stuff...

--- hxd/fmt/hmd/Data.hx
+++ hxd/fmt/hmd/Data.hx
@@ -9,4 +9,30 @@ package hxd.fmt.hmd;
 	public var DBytes4 = 9;
 
 	inline function new(v) {
-		this
+		this = v;
+	}
+
+	public inline function getSize() {
+		return this & 7;
+	}
+
+	public inline function toInt() {
+		return this;
+	}
+
+	public function toString() {
+		return switch( new GeometryDataFormat(this) ) {
+		case DFloat: "DFloat";
+		case DVec2: "DVec2";
+		case DVec3: "DVec3";
+		case DVec4: "DVec4";
+		case DBytes4: "DBytes4";
+		}
+	}
+
+	public static inline function fromInt( v : Int ) : GeometryDataFormat {
+		return new GeometryDataFormat(v);
+	}
+}
+
+typedef DataPosition = Int
