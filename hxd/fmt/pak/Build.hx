685534fd722c575c002ed1d1f27732995c1a85a0
inboxready <112681018+inboxready@users.noreply.github.com>
2021-08-11 06:53:08 +0000

    upravy

--- hxd/fmt/pak/Build.hx
+++ hxd/fmt/pak/Build.hx
@@ -4,4 +4,21 @@ import hxd.fmt.pak.Data;
 class Build {
 
 	var fs : hxd.fs.LocalFileSystem;
-	var out : { bytes : Array<haxe.
+	var out : { bytes : Array<haxe.io.Bytes>, size : Float };
+	var configuration : String;
+	var nextPath : String;
+
+	public var excludedExt : Array<String> = [];
+	public var excludedNames : Array<String> = [];
+	public var excludePath : Array<String> = [];
+	public var includePath : Array<String> = [];
+	public var resPath : String = "res";
+	public var outPrefix : String;
+	public var pakDiff = false;
+	public var checkJPG = false;
+	public var checkOGG = false;
+
+	function new() {
+	}
+
+	function c
