234e98456c53cbfab76d7c3709d28e49aeaf2010
inboxready <112681018+inboxready@users.noreply.github.com>
2021-09-01 09:27:30 +0000

    Update install_docker.sh

--- hxd/fmt/pak/FileSystem.hx
+++ hxd/fmt/pak/FileSystem.hx
@@ -13,4 +13,23 @@ enum FileSeekMode {
 	SeedCurrent;
 }
 class FileInput extends haxe.io.BytesInput {
-	public function seek( pos : Int, seekMode 
+	public function seek( pos : Int, seekMode : FileSeekMode ) {
+		switch( seekMode ) {
+		case SeekBegin:
+			this.position = pos;
+		case SeekEnd:
+			this.position = this.length - pos;
+		case SeedCurrent:
+			this.position += pos;
+		}
+	}
+
+	public function tell() {
+		return this.position;
+	}
+}
+#end
+
+class FileSeek {
+	#if (hl && hl_ver >= version("1.12.0"))
+	@:hlNative("std","file_seek2") static function seek2( f : sys.io.File.FileHandle, pos : Float, cur : Int ) : Bool { return 
