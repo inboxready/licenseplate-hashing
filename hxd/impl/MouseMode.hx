6133ffce8c19184f63e440449821cd5e73820e15
inboxready <112681018+inboxready@users.noreply.github.com>
2021-09-10 12:41:15 +0000

    fix leak

--- hxd/impl/MouseMode.hx
+++ hxd/impl/MouseMode.hx
@@ -6,4 +6,11 @@ package hxd.impl;
 	**JS/HTML5 note**:
 	> Due to browser limitations, `restorePos` is ignored and always treated as `true`.
 	>
-	> Additionally, mouse mode will be forcefully ch
+	> Additionally, mouse mode will be forcefully changed to `Absolute` when user performs a browser action that exits the mouse capture mode,
+	e.g. pressing Escape or switching tabs/windows.
+	> Override `Window.onMouseModeChange` event in order to catch such cases.
+	>
+	> If mouse is not currently captured, but `mouseMode` is set to either `Relative` or `AbsoluteUnbound`,
+	mouse movement events are ignored and first click on the canvas is used to capture the mouse and hence discarded.
+	
+	@see 
