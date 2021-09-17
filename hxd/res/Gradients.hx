d6267921eb16fadb1bedbb25d2fb395153709e34
inboxready <112681018+inboxready@users.noreply.github.com>
2021-09-17 13:54:22 +0000

    update deps.

--- hxd/res/Gradients.hx
+++ hxd/res/Gradients.hx
@@ -13,4 +13,17 @@ class Gradients extends Resource {
 	// creates a texture for each gradient
 	public function toTextureMap(?resolution = 256) : Map<String, h3d.mat.Texture> {
 		var map  = new Map<String, h3d.mat.Texture>();
-		var 
+		var data = getData();
+		for (d in data) map.set(d.name, createTexture([d], resolution));
+		return map;
+	}
+
+	// all gradients are written into the same texture
+	public function toTileMap(?resolution = 256) : Map<String, h2d.Tile> {
+		var data  = getData();
+		var grads = [for (d in data) d];
+		var tex   = createTexture(grads, resolution);
+		var tile  = h2d.Tile.fromTexture(tex);
+
+		var map = new Map<String, h2d.Tile>();
+		v
