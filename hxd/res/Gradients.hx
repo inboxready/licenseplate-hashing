85374a98144b217b0964ac001e892f2e72cff0c3
inboxready <112681018+inboxready@users.noreply.github.com>
2021-09-17 10:38:12 +0000

    Added missing files

--- hxd/res/Gradients.hx
+++ hxd/res/Gradients.hx
@@ -1,4 +1,16 @@
 package hxd.res;
 import hxd.fmt.grd.Data;
 
-c
+class Gradients extends Resource {
+	var data : Data;
+
+	// creates a texture for the specified "name" gradient
+	public function toTexture(name : String, ?resolution = 256) : h3d.mat.Texture {
+		var data = getData();
+		return createTexture([data.get(name)], resolution);
+	}
+
+	// creates a texture for each gradient
+	public function toTextureMap(?resolution = 256) : Map<String, h3d.mat.Texture> {
+		var map  = new Map<String, h3d.mat.Texture>();
+		var 
