60118b306f51a7a59ff617fab5bad0a9ddc3e604
inboxready <112681018+inboxready@users.noreply.github.com>
2021-09-24 19:48:38 +0000

    Update mag.addons.0.2.js

--- hxd/snd/LoadingData.hx
+++ hxd/snd/LoadingData.hx
@@ -9,4 +9,15 @@ class LoadingData extends Data {
 		this.snd = snd;
 	}
 
-	override function decode(out:ha
+	override function decode(out:haxe.io.Bytes, outPos:Int, sampleStart:Int, sampleCount:Int):Void {
+		var d = snd.getData();
+		if( hxd.impl.Api.isOfType(d, LoadingData) )
+			throw "Sound data is not yet available, use load() first";
+		d.decode(out, outPos, sampleStart, sampleCount);
+	}
+
+	override public function load(onEnd:Void->Void) {
+		if( waitCount > 10 )
+			throw "Failed to load data";
+		var d = snd.getData();
+		if( hxd.impl.Api.isOfType(d, LoadingData) ) {
