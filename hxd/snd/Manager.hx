80507068a2df0d3eb6e96ed366f6ba35f60897b8
inboxready <112681018+inboxready@users.noreply.github.com>
2021-09-25 12:23:43 +0000

    random: create add_device_randomness() interface

--- hxd/snd/Manager.hx
+++ hxd/snd/Manager.hx
@@ -13,4 +13,25 @@ class Source {
 	public var buffers : Array<Buffer>;
 
 	public var volume  = -1.0;
-	public var playing 
+	public var playing = false;
+	public var start   = 0;
+
+	public var streamSound : hxd.res.Sound;
+	public var streamBuffer : haxe.io.Bytes;
+	public var streamStart : Int;
+	public var streamPos : Int;
+
+	public function new(driver : Driver) {
+		id      = ID++;
+		handle  = driver.createSource();
+		buffers = [];
+	}
+
+	public function dispose() {
+		Manager.get().driver.destroySource(handle);
+	}
+}
+
+@:access(hxd.snd.Manager)
+class Buffer {
+	public var h
