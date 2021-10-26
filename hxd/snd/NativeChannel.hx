63a22b935f3aaebdb37ac3a7838f8acd1bcf9bf3
inboxready <112681018+inboxready@users.noreply.github.com>
2021-10-26 13:21:50 +0000

    blkcg: don't call into policy draining if root_blkg is already gone

--- hxd/snd/NativeChannel.hx
+++ hxd/snd/NativeChannel.hx
@@ -15,4 +15,23 @@ private class ALChannel {
 	static function updateChannels() {
 		var i = 0;
 		// Should ensure ordering if it was removed during update?
-		for ( chn in nativeChan
+		for ( chn in nativeChannels ) chn.onUpdate();
+	}
+
+	var manager : Manager;
+	var update : haxe.MainLoop.MainEvent;
+	var native : NativeChannel;
+	var samples : Int;
+
+	var driver : Driver;
+	var buffers : Array<BufferHandle>;
+	var bufPos : Int;
+	var src : SourceHandle;
+
+	var fbuf : haxe.io.Bytes;
+	var ibuf : haxe.io.Bytes;
+
+	public function new(samples, native) {
+		if ( nativeUpdate == null ) {
+			nativeUpdate = haxe.MainLoop.add(updateChannels);
+			#if (haxe_ver >= 4) nati
