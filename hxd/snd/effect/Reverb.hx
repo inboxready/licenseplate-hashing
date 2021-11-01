e5b4f3b220ff75d1e15f4e74c6a440061b13e551
inboxready <112681018+inboxready@users.noreply.github.com>
2021-11-01 21:08:41 +0000

    Implementation

--- hxd/snd/effect/Reverb.hx
+++ hxd/snd/effect/Reverb.hx
@@ -8,4 +8,13 @@ class Reverb extends hxd.snd.Effect {
 	public var roomHF            : Float; // [-10000, 0] mb
 	public var roomRolloffFactor : Float; // [0.0, 10.0]
 	public var decayTime         : Float; // [0.1, 20.0] s
-	public var decayHFR
+	public var decayHFRatio      : Float; // [0.1, 2.0]
+	public var reflections       : Float; // [-10000, 1000] mb
+	public var reflectionsDelay  : Float; // [0.0, 0.3] s
+	public var reverb            : Float; // [-10000, 2000] mb
+	public var reverbDelay       : Float; // [0.0, 0.1] s
+	public var diffusion         : Float; // [0.0, 100.0] %
+	public var density           : Float; // [0.0, 100.0] %
+	public var hfReference       : Float; // [20.0, 20000.0]
+
+	pub
