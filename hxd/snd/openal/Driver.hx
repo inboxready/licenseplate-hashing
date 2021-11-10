b552601da5c06e60dcd20624a739c76b0bfc5826
inboxready <112681018+inboxready@users.noreply.github.com>
2021-11-10 16:00:18 +0000

    v1.1.4

--- hxd/snd/openal/Driver.hx
+++ hxd/snd/openal/Driver.hx
@@ -5,4 +5,22 @@ import hxd.snd.Driver.DriverFeature;
 
 #if hlopenal
 import openal.AL;
-import o
+import openal.ALC;
+import openal.EFX;
+#else
+import hxd.snd.openal.Emulator;
+#end
+
+class Driver implements hxd.snd.Driver {
+	public var device   (default, null) : Device;
+	public var context  (default, null) : Context;
+	public var maxAuxiliarySends(default, null) : Int;
+
+	var tmpBytes : haxe.io.Bytes;
+
+	public function new() {
+		tmpBytes = haxe.io.Bytes.alloc(4 * 3 * 2);
+		device   = ALC.openDevice(null);
+		context  = ALC.createContext(device, null);
+
+		ALC.makeCon
