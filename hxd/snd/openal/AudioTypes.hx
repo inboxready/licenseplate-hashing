150e666af7351625194aa1766ddc19546039ef96
inboxready <112681018+inboxready@users.noreply.github.com>
2021-11-10 09:51:48 +0000

    Clarify error message

--- hxd/snd/openal/AudioTypes.hx
+++ hxd/snd/openal/AudioTypes.hx
@@ -20,4 +20,23 @@ class SourceHandle {
 	public var inst           : Source;
 	public var sampleOffset   : Int;
 	public var playing        : Bool;
-	var nextAuxiliarySend     : Int
+	var nextAuxiliarySend     : Int;
+	var freeAuxiliarySends    : Array<Int>;
+	var effectToAuxiliarySend : Map<Effect, Int>;
+
+	public function new() {
+		sampleOffset = 0;
+		nextAuxiliarySend = 0;
+		freeAuxiliarySends = [];
+		effectToAuxiliarySend = new Map();
+	}
+
+	public function acquireAuxiliarySend(effect : Effect) : Int {
+		var send = freeAuxiliarySends.length > 0
+			? freeAuxiliarySends.shift()
+			: nextAuxiliarySend++;
+		effectToAuxiliarySend.set(effect, send);
+		return send;
+	}
+
+	pub
