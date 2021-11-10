d278a4e92db01831813539607b04ab0544791e2e
inboxready <112681018+inboxready@users.noreply.github.com>
2021-11-10 06:52:23 +0000

    USB: xhci - fix bit definitions for IMAN register

--- hxd/snd/openal/AudioTypes.hx
+++ hxd/snd/openal/AudioTypes.hx
@@ -1,4 +1,23 @@
 package hxd.snd.openal;
 
 #if hlopenal
-i
+import openal.AL.Source;
+import openal.AL.Buffer;
+typedef AL = openal.AL;
+typedef EFX = openal.EFX;
+#else
+import hxd.snd.openal.Emulator;
+typedef AL = Emulator;
+#end
+
+class BufferHandle {
+	public var inst : Buffer;
+	public var isEnd : Bool;
+	public function new() { }
+}
+
+class SourceHandle {
+	public var inst           : Source;
+	public var sampleOffset   : Int;
+	public var playing        : Bool;
+	var nextAuxiliarySend     : Int
