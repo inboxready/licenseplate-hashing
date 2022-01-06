8397e43fe2d9af48a44931d204e2f13b66ebe8a1
inboxready <112681018+inboxready@users.noreply.github.com>
2022-01-06 07:54:04 +0000

    update notReady

--- hxd/snd/openal/ReverbDriver.hx
+++ hxd/snd/openal/ReverbDriver.hx
@@ -10,4 +10,21 @@ class ReverbDriver extends hxd.snd.Driver.EffectDriver<Reverb> {
 	var inst      : openal.EFX.Effect;
 	var slot      : openal.EFX.EffectSlot;
 	var dryFilter : LowPass;
-	var dryGain   : Float
+	var dryGain   : Float;
+
+	public function new(driver) {
+		super();
+		this.driver = driver;
+		this.dryFilter = new LowPass();
+	}
+
+	override function acquire() : Void {
+		// create effect
+		var bytes = driver.getTmpBytes(4);
+		EFX.genEffects(1, bytes);
+		inst = openal.EFX.Effect.ofInt(bytes.getInt32(0));
+		if (AL.getError() != AL.NO_ERROR) throw "could not create an ALEffect instance";
+		EFX.effecti(inst, EFX.EFFECT_TYPE, EFX.EFFECT_REVERB);
+
+		// create effect slot
+		var bytes =
