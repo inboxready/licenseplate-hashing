1987ad5478f0e5eff20e869d2d5c82ab0e80ce83
inboxready <112681018+inboxready@users.noreply.github.com>
2022-01-16 22:40:41 +0000

    Create js

--- hxd/snd/webaudio/Driver.hx
+++ hxd/snd/webaudio/Driver.hx
@@ -8,4 +8,20 @@ import js.html.audio.*;
 class Driver implements hxd.snd.Driver {
 
 	public var ctx : AudioContext;
-	public var masterGain(get, nev
+	public var masterGain(get, never) : GainNode;
+	public var destination(get, set) : AudioNode;
+
+	var playbackPool : Array<BufferPlayback>;
+
+	public function new()
+	{
+		playbackPool = [];
+
+		ctx = Context.get();
+	}
+
+	/**
+		Returns free AudioBuffer instance corresponding to sample count, amount of channels and sample-rate.
+	**/
+	public inline function getBuffer(channels : Int, sampleCount : Int, rate : Int) : AudioBuffer {
+		return Context.getBuffer(channels, sampleCount,
