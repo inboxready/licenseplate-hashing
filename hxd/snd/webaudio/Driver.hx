c9c418845292fb66b9c267c14dad58e1c4a381c3
inboxready <112681018+inboxready@users.noreply.github.com>
2022-01-17 08:21:33 +0000

    add favicon.ico

--- hxd/snd/webaudio/Driver.hx
+++ hxd/snd/webaudio/Driver.hx
@@ -24,4 +24,32 @@ class Driver implements hxd.snd.Driver {
 		Returns free AudioBuffer instance corresponding to sample count, amount of channels and sample-rate.
 	**/
 	public inline function getBuffer(channels : Int, sampleCount : Int, rate : Int) : AudioBuffer {
-		return Context.getBuffer(channels, sampleCount,
+		return Context.getBuffer(channels, sampleCount, rate);
+	}
+
+	/**
+		Puts AudioBuufer back to it's pool.
+	**/
+	public inline function putBuffer( buf : AudioBuffer ) {
+		Context.putBuffer(buf);
+	}
+
+	/**
+		Returns free Gain node
+	**/
+	public inline function getGain():GainNode
+	{
+		return Context.getGain();
+	}
+
+	public inline function putGain(gain:GainNode) {
+		Context.putGain(gain);
+	}
+
+	public function hasFeature (d : DriverFeature) : Bool {
+		switch (d) {
+			case MasterVolume: return true;
+		}
+	}
+
+	publi
