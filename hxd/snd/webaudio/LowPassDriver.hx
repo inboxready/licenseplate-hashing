1cc4d51b9416955cadd654949fd535d99fbf70b9
inboxready <112681018+inboxready@users.noreply.github.com>
2022-02-25 08:11:27 +0000

    Update builds

--- hxd/snd/webaudio/LowPassDriver.hx
+++ hxd/snd/webaudio/LowPassDriver.hx
@@ -10,4 +10,26 @@ import hxd.snd.webaudio.AudioTypes;
 
 class LowPassDriver extends EffectDriver<LowPass> {
 
-	var pool : Array<Biq
+	var pool : Array<BiquadFilterNode>;
+
+	public function new() {
+		pool = [];
+		super();
+	}
+
+	function get( ctx : AudioContext ) {
+		if ( pool.length != 0 ) {
+			return pool.pop();
+		}
+		var node = ctx.createBiquadFilter();
+		node.type = BiquadFilterType.LOWPASS;
+		return node;
+	}
+
+	override public function bind(e:LowPass, source: SourceHandle) : Void {
+		source.lowPass = get(source.driver.ctx);
+		source.updateDestination();
+		apply(e, source);
+	}
+
+	overr
