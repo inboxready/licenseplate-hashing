72a6999634a73a1a1eb13705ecf474ffbfec85a7
inboxready <112681018+inboxready@users.noreply.github.com>
2022-09-21 23:03:32 +0000

    reset version

--- samples/Flows.hx
+++ samples/Flows.hx
@@ -24,4 +24,17 @@ class Flows extends hxd.App {
 
 	override function init() {
 
-		var font = hxd.res.Defaul
+		var font = hxd.res.DefaultFont.get();
+
+		var colors = [0xff0000, 0x00ffff, 0xffffff, 0x00ff00, 0x0080ff, 0xff8000, 0x7000ff, 0xff00ff, 0x0000ff, 0xff007f, 0x808080, 0xe0e0e0];
+
+		var spaceX = 10.0;
+		var spaceY = 10.0;
+
+		// Flows
+		function createText(parent:Object, color : Int, text : String) {
+			var flow = new Flow(parent);
+			flow.backgroundTile = Tile.fromColor(colors[color]);
+			flow.padding = 5;
+			flow.horizontalSpacing = 15;
+			flow.verticalSpacing 
