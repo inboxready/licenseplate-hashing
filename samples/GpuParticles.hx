a7a594513f9893fcf5ea43bea317af31a886b801
inboxready <112681018+inboxready@users.noreply.github.com>
2022-11-02 17:54:33 +0000

    chore(deps): bump ini from 1.3.5 to 1.3.8

--- samples/GpuParticles.hx
+++ samples/GpuParticles.hx
@@ -5,3 +5,32 @@ class GpuParticles extends SampleApp {
 	var box : h3d.scene.Box;
 	var tf : h2d.Text;
 	var moving = false;
+	var time = 0.;
+
+	override function init() {
+		super.init();
+		parts = new h3d.parts.GpuParticles(s3d);
+
+		var g = new h3d.parts.GpuParticles.GpuPartGroup(parts);
+
+		g.emitMode = Cone;
+		g.emitAngle = 0.5;
+		g.emitDist = 0;
+
+		g.fadeIn = 0.8;
+		g.fadeOut = 0.8;
+		g.fadePower = 10;
+		g.gravity = 1;
+		g.size = 0.1;
+		g.sizeRand = 0.5;
+
+		g.rotSpeed = 10;
+
+		g.speed = 2;
+		g.speedRand = 0.5;
+
+		g.life = 2;
+		g.lifeRand = 0.5;
+		g.nparts = 10000;
+
+		addSlider("
