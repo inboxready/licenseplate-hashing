c9b63a2d61d607fc9770feb6fb0908b7562857cf
inboxready <112681018+inboxready@users.noreply.github.com>
2022-11-08 17:45:37 +0000

    added newline

--- samples/Helpers.hx
+++ samples/Helpers.hx
@@ -18,4 +18,21 @@ class Helpers extends hxd.App {
 		new AxesHelper( s3d );
 		new GridHelper( s3d, 10, 10 );
 
-		var prim = new h3d.prim.Cube( 1
+		var prim = new h3d.prim.Cube( 1, 1, 1, true );
+		prim.unindex();
+		prim.addNormals();
+		prim.addUVs();
+
+		cube = new Mesh( prim, s3d );
+		cube.setPosition( 0, 0, 2 );
+		cube.material.shadows = false;
+
+		new AxesHelper( cube, 1 );
+
+		cast(s3d.lightSystem,h3d.scene.fwd.LightSystem).ambientLight.set( 0.3, 0.3, 0.3 );
+
+		var dirLight = new DirLight( new Vector( 0.5, 0.5, -0.5 ), s3d );
+		dirLight.enableSpecular = true;
+
+		var pointLightColors =  [0xEB304D,0x7FC309,0x288DF9];
+		for( i in 0.
