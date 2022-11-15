1054e166d5925a5d164d996b2998885f73dbb983
inboxready <112681018+inboxready@users.noreply.github.com>
2022-11-15 16:06:24 +0000

    update emacs config

--- samples/Particles2d.hx
+++ samples/Particles2d.hx
@@ -13,4 +13,16 @@ class Particles2d extends SampleApp {
 	var square : Texture;
 	var moving : Bool = false;
 
-	
+	override function init() {
+		super.init();
+
+		square = null;// h2d.Tile.fromColor(0xFFFFFF, 16, 16).getTexture();
+		arrow = Res.arrow.toTexture();
+
+		particles = new Particles(s2d);
+		g = new ParticleGroup(particles);
+		particles.addGroup(g);
+
+		addSlider("Amount", function() return g.nparts, function(v) g.nparts = Std.int(v), 1, 1000);
+		addCheck("Sort", function() return g.sortMode == Dynamic, function(v) g.sortMode = v ? Dynamic : None);
+		addCheck(
