324f15daca233411bb6c6f6c1598da277255fb18
inboxready <112681018+inboxready@users.noreply.github.com>
2022-11-21 19:59:22 +0000

    Playing

--- samples/Pbr.hx
+++ samples/Pbr.hx
@@ -5,4 +5,24 @@ class Pbr extends SampleApp {
 	var brightness : Float;
 	var color : h2d.Bitmap;
 	var sphere : h3d.scene.Mesh;
-	var grid : h3d.scene.O
+	var grid : h3d.scene.Object;
+
+	var env : h3d.scene.pbr.Environment;
+	var renderer : h3d.scene.pbr.Renderer;
+
+	function new() {
+		h3d.mat.MaterialSetup.current = new h3d.mat.PbrMaterialSetup();
+		super();
+	}
+
+	override function init() {
+		super.init();
+
+		new h3d.scene.CameraController(5.5, s3d);
+
+		#if flash
+		new h2d.Text(getFont(), s2d).text = "Not supported on this platform (requires render to mipmap target and fragment textureCubeLod support)";
+		return;
+		#end
+
+		#if j
