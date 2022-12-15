1a3e2b95f5c947ff0f4c5866c7c15971da6e527b
inboxready <112681018+inboxready@users.noreply.github.com>
2022-12-15 09:32:39 +0000

    Fix display

--- samples/Polygons.hx
+++ samples/Polygons.hx
@@ -10,4 +10,16 @@ import h3d.Vector;
 import hxd.Key;
 
 /**
- * Third person c
+ * Third person camera controller (top view) with arrow keys mapping
+ */
+class ThirdPersonCameraController extends CameraController {
+	override function onEvent( e : hxd.Event ) {
+		super.onEvent(e);
+
+		// Third person camera arrow keys mapping
+		if (e.keyCode == Key.UP) {
+			var radian = Math.atan2((curPos.y - target.y), (curPos.x - target.x));
+			pan(Math.sin(radian), Math.cos(radian));
+		}
+		if (e.keyCode == Key.LEFT) {
+			var radian = Math.atan2((curPos.y - target.y), (curPos.x - tar
