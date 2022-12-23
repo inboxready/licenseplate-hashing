fdee183877267fe89ed1d6b607b73cadcdb8830a
inboxready <112681018+inboxready@users.noreply.github.com>
2022-12-23 15:27:23 +0000

    adreno_idler: fix-up some comments

--- samples/Video.hx
+++ samples/Video.hx
@@ -1,4 +1,26 @@
 
 class Video extends hxd.App {
 
-	var v
+	var video : h2d.Video;
+	var tf : h2d.Text;
+
+	override function init() {
+		tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
+		video = new h2d.Video(s2d);
+		video.onError = function(e) {
+			tf.text = e;
+			tf.textColor = 0xFF0000;
+		};
+		function start() {
+			#if hl
+			video.load("testVideo.avi");
+			#elseif js
+			video.load("testVideo.mp4");
+			#end
+		}
+		video.onEnd = start;
+		start();
+	}
+
+	override function update(dt:Float) {
+	
